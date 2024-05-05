db214769ffcc7feaa8b99094f44a0f244f2a1a08
summerdawn04 <157253657+summerdawn04@users.noreply.github.com>
2024-05-05 17:00:26 -0500

    update gutter

--- js/gallery.js
+++ js/gallery.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
             display: false
         },
         thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|labelAppear75",
-        thumbnailGutterWidth: 0,
-        thumbnailGutterHeight: 0,
+        thumbnailGutterWidth: 20,
+        thumbnailGutterHeight: 20,
         thumbnailAlignment: "center",
         thumbnailOpenImage: true
 
