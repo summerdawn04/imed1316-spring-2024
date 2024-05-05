f9427c4fb33eb29cbde29dd480031b14d98ccad7
summerdawn04 <157253657+summerdawn04@users.noreply.github.com>
2024-05-05 16:37:18 -0500

    added tags

---
+++ js/gallery.js
@@ -0,0 +1,12 @@
+
+// A $( document ).ready() block.
+$(document).ready(function () {
+    //console.log("ready!");
+
+    $("#blinn-gallery").nanogallery2({
+        ...
+        thumbnailHeight: 200,
+        thumbnailWidth: 200
+    });
+
+});
